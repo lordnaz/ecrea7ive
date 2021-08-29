c9114d43c85e44d261e4d38dbb938c03e0cb83c4
Nazrul Hanif <nazrul@kalsym.com>
2021-08-30 04:40:14 +0800

    hotfix StockList Model caller in InventoryController

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\Stocklist;
+use App\Models\StockList as Stocklist;
 use App\Models\StockUpdateTrail;
 
 use Increment;
